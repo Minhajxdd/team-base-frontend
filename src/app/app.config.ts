41a6ec0483d57e4d61d1b7cd84b7f58bcf26a0cb
Mohammed Minhaj <nijulinux@gmail.com>
2025-01-08 13:56:01 +0530

    config(http-client): added http client in appconfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,18 +7,18 @@ import { authRoutes } from './core/auth/auth.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
-
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter([...authRoutes, ...routes]),
+    provideHttpClient(),
     provideAnimationsAsync(),
-        providePrimeNG({
-            theme: {
-                preset: Aura
-            }
-        })
-
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
   ],
 };
