2edc0ad278329dde2118de1a22cefbe93a08db98
Mohammed Minhaj <nijulinux@gmail.com>
2025-01-10 18:02:15 +0530

    feat(user-nav-bar): navbar toggle & logout feauture added

--- src/app/shared/components/navbar/user-navbar/user-navbar.component.ts
+++ src/app/shared/components/navbar/user-navbar/user-navbar.component.ts
@@ -1,17 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ThemeModeService } from '../../../../core/services/theme.service';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../../core/auth/auth.service';
 
 @Component({
   selector: 'app-user-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-navbar.component.html',
-  styleUrl: './user-navbar.component.css'
+  styleUrl: './user-navbar.component.css',
 })
 export class UserNavbarComponent {
   private readonly themeService = inject(ThemeModeService);
+  private readonly authService = inject(AuthService);
+
   isDarkMode = this.themeService.$isDarkMode;
+  isDropActive = signal(false);
 
   onToggle() {
     this.themeService.toggle();
   }
+
+  onProfileBtn() {
+    this.isDropActive.set(!this.isDropActive());
+  }
+
+  onLogOut() {
+    this.authService.logout();
+  }
+
 }
