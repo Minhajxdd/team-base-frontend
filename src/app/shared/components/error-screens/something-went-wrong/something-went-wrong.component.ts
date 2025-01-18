2b7be6d08182a1d3211159fbf0eecd0496bda613
Mohammed Minhaj <nijulinux@gmail.com>
2025-01-18 21:52:32 +0530

    feat(utils): somethign went wrong componetn added

---
+++ src/app/shared/components/error-screens/something-went-wrong/something-went-wrong.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-something-went-wrong',
+  imports: [],
+  templateUrl: './something-went-wrong.component.html',
+  styleUrl: './something-went-wrong.component.css',
+})
+export class SomethingWentWrongComponent {}
