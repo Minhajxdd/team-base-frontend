9abf450cd0dedf1ef27c0fd7076f145bf1dc4fac
Mohammed Minhaj <nijulinux@gmail.com>
2025-01-20 21:54:21 +0530

    fix(project-tasked): add tasks role added

--- src/app/features/project/pages/project-tasks/header/header.component.ts
+++ src/app/features/project/pages/project-tasks/header/header.component.ts
@@ -1,13 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { AddTaskFormComponent } from "./add-task-form/add-task-form.component";
+import { Component, DestroyRef, signal } from '@angular/core';
+import { AddTaskFormComponent } from './add-task-form/add-task-form.component';
+import { Store } from '@ngrx/store';
+import { selectProjectRole } from '../../../store/project.selector';
 
 @Component({
   selector: 'app-header',
   imports: [AddTaskFormComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  role!: string;
+
+  constructor(private store: Store, private destroyRef: DestroyRef) {
+    const subscription = this.store.select(selectProjectRole).subscribe((data) => {
+      if(data)
+        this.role = data;
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+
   isFormVisible = signal(false);
 
   onFormOpen() {
@@ -16,5 +30,5 @@ export class HeaderComponent {
 
   onFormClose(value: boolean) {
     this.isFormVisible.set(value);
-  } 
+  }
 }
