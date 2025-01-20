88935d52ac1c78ada565f577d8801aa78c68de9e
Mohammed Minhaj <nijulinux@gmail.com>
2025-01-20 11:41:28 +0530

    feat(project): project card role taken from ngrx store

--- src/app/features/project/pages/project-members/project-members-display/profile-card/profile-card.component.ts
+++ src/app/features/project/pages/project-members/project-members-display/profile-card/profile-card.component.ts
@@ -1,6 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, DestroyRef, input, OnInit } from '@angular/core';
 import { projectMember } from '../project-members.dispaly.model';
 import { RoleChangeFormComponent } from './role-change-form/role-change-form.component';
+import { Store } from '@ngrx/store';
+import { selectProjectRole } from '../../../../store/project.selector';
 
 @Component({
   selector: 'app-profile-card',
@@ -8,10 +10,26 @@ import { RoleChangeFormComponent } from './role-change-form/role-change-form.com
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.css',
 })
-export class ProfileCardComponent {
+export class ProfileCardComponent implements OnInit {
   member = input.required<projectMember>();
 
   isEditFormVisible = false;
+
+  projectRole: string | null = null;
+
+  constructor(private store: Store, private destoryRef: DestroyRef) {}
+
+  ngOnInit(): void {
+    const subscription = this.store
+      .select(selectProjectRole)
+      .subscribe((data) => {
+        this.projectRole = data;
+      });
+
+    this.destoryRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 
   onOpenForm() {
     this.isEditFormVisible = true;
