27fc6d6f13035dadb136262909e9af126ed69895
Mohammed Minhaj <nijulinux@gmail.com>
2025-01-21 09:11:56 +0530

    fix(project): project components lazy loaded

--- src/app/features/project/project.routes.ts
+++ src/app/features/project/project.routes.ts
@@ -2,9 +2,6 @@ import { Routes } from '@angular/router';
 import { ProjectTasksComponent } from './pages/project-tasks/project-tasks.component';
 import { ProjectRolesGuard } from '../../core/guards/project.guard';
 import { UserAuthGuard } from '../../core/guards/user.guard';
-import { ProjectMembersComponent } from './pages/project-members/project-members.component';
-import { ProjectRequestAcceptComponent } from './pages/project-request-accept/project-request-accept.component';
-import { ProjectDashboardComponent } from './pages/project-dashboard/project-dashboard.component';
 
 export const projectRoutes: Routes = [
   {
@@ -12,22 +9,34 @@ export const projectRoutes: Routes = [
     children: [
       {
         path: ':projectId/dashboard',
-        component: ProjectDashboardComponent,
+        loadComponent: () =>
+          import('./pages/project-dashboard/project-dashboard.component').then(
+            (m) => m.ProjectDashboardComponent
+          ),
         canActivate: [UserAuthGuard, ProjectRolesGuard],
       },
       {
         path: ':projectId/members',
-        component: ProjectMembersComponent,
+        loadComponent: () =>
+          import('./pages/project-members/project-members.component').then(
+            (m) => m.ProjectMembersComponent
+          ),
         canActivate: [UserAuthGuard, ProjectRolesGuard],
       },
       {
         path: ':projectId/members/accept-request',
-        component: ProjectRequestAcceptComponent,
+        loadComponent: () =>
+          import(
+            './pages/project-request-accept/project-request-accept.component'
+          ).then((m) => m.ProjectRequestAcceptComponent),
         canActivate: [UserAuthGuard],
       },
       {
         path: ':projectId/tasks',
-        component: ProjectTasksComponent,
+        loadComponent: () =>
+          import('./pages/project-tasks/project-tasks.component').then(
+            (m) => m.ProjectTasksComponent
+          ),
         canActivate: [UserAuthGuard, ProjectRolesGuard],
       },
     ],
