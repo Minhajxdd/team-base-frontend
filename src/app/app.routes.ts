c3cdb5b8941f490e170f84effb7f3fd5139df106
Mohammed Minhaj <nijulinux@gmail.com>
2025-01-29 12:19:50 +0530

    feat(home-profile): added home profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { UserAuthGuard } from './core/guards/user.guard';
 import { HomeComponent } from './features/home/home-page/home.component';
+import { ProfileEditComponent } from './features/home/profile-edit/profile-edit.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    canActivate: [UserAuthGuard],
+  },
+  {
+    path: 'profile',
+    component: ProfileEditComponent,
     canActivate: [UserAuthGuard],
   },
 ];
