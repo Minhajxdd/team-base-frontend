be3a65ad44aafc9b2920906a583270cd0a27ae6f
Mohammed Minhaj <nijulinux@gmail.com>
2025-01-30 13:05:22 +0530

    feat(user-navbar): retreed data and implemented on the navbar

--- src/app/shared/components/navbar/navbar.model.ts
+++ src/app/shared/components/navbar/navbar.model.ts
@@ -1,3 +1,10 @@
+export type NavBarTypeModel = null | 'user' | 'admin' | 'project';
 
-
-export type NavBarTypeModel = null | 'user' | 'admin' | 'project'
+export interface User {
+  id: string | null;
+  fullName: string | null;
+  email: string | null;
+  profile: string | null;
+  loading: boolean;
+  error: string | null;
+}
--- src/app/shared/components/navbar/user-navbar/user-navbar.component.ts
+++ src/app/shared/components/navbar/user-navbar/user-navbar.component.ts
@@ -1,8 +1,16 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnDestroy, signal } from '@angular/core';
 import { ThemeModeService } from '../../../../core/services/theme.service';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../../core/auth/auth.service';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import {
+  selectUser,
+  selectUserLoading,
+} from '../../../store/user/user.selector';
+import { loadUser } from '../../../store/user/user.actions';
+import { User } from '../navbar.model';
 
 @Component({
   selector: 'app-user-navbar',
@@ -10,9 +18,35 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './user-navbar.component.html',
   styleUrl: './user-navbar.component.css',
 })
-export class UserNavbarComponent {
+export class UserNavbarComponent implements OnDestroy {
   private readonly themeService = inject(ThemeModeService);
   private readonly authService = inject(AuthService);
+
+  user$: Observable<any>;
+  loading$: Observable<boolean>;
+
+  constructor(private readonly store: Store) {
+    this.user$ = this.store.select(selectUser);
+    this.loading$ = this.store.select(selectUserLoading);
+  }
+
+  subscription1$!: Subscription;
+  subscription2$!: Subscription;
+
+  isLoading = signal<boolean | null>(false);
+  user = signal<User | null>(null);
+
+  ngOnInit() {
+    this.store.dispatch(loadUser());
+
+    this.subscription1$ = this.user$.subscribe((userData) => {
+      this.user.set(userData);
+    });
+
+    this.subscription2$ = this.loading$.subscribe((loadingData) => {
+      this.isLoading.set(loadingData);
+    });
+  }
 
   isDarkMode = this.themeService.$isDarkMode;
   isDropActive = signal(false);
@@ -29,4 +63,8 @@ export class UserNavbarComponent {
     this.authService.logout();
   }
 
+  ngOnDestroy(): void {
+    this.subscription1$!.unsubscribe();
+    this.subscription2$!.unsubscribe();
+  }
 }
