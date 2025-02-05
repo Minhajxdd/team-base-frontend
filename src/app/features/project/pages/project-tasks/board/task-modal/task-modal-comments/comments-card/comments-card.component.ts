2cd11964067ad54cd1e77462e1293caee65eac21
Mohammed Minhaj <nijulinux@gmail.com>
2025-02-05 10:39:53 +0530

    feat(comments): added ui roles for comments

--- src/app/features/project/pages/project-tasks/board/task-modal/task-modal-comments/comments-card/comments-card.component.ts
+++ src/app/features/project/pages/project-tasks/board/task-modal/task-modal-comments/comments-card/comments-card.component.ts
@@ -1,22 +1,26 @@
 import { Component, ElementRef, input, output } from '@angular/core';
 import { Comments } from '../../task-modal.model';
+import { ProjectRoleService } from '../../../../../../shared/service/project.role.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-comments-card',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './comments-card.component.html',
   styleUrl: './comments-card.component.css',
 })
 export class CommentsCardComponent {
-  constructor(private ele: ElementRef) {}
+  constructor(
+    private ele: ElementRef,
+    protected projectRoleService: ProjectRoleService
+  ) {}
 
   comment = input.required<Comments | undefined>();
   deleteComment = output<string>();
 
   onDelete() {
     const comment = this.comment()?._id;
-    if(comment)
-    this.deleteComment.emit(comment);
-    this.ele.nativeElement.remove()
+    if (comment) this.deleteComment.emit(comment);
+    this.ele.nativeElement.remove();
   }
 }
