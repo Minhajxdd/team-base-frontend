3a5296f5b0fe267fe19574a0ed5ff852841c78f1
Mohammed Minhaj <nijulinux@gmail.com>
2025-02-19 12:17:54 +0530

    revert(project-video-call): removed wevrtc video call

--- src/app/features/project/pages/project-video-call/project-video-call.component.ts
+++ src/app/features/project/pages/project-video-call/project-video-call.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { VideoCallsCardComponent } from "./video-calls-card/video-calls-card.component";
 
 @Component({
   selector: 'app-project-video-call',
-  imports: [VideoCallsCardComponent],
+  imports: [],
   templateUrl: './project-video-call.component.html',
   styleUrl: './project-video-call.component.css'
 })
