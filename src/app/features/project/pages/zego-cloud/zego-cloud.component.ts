07d75fa2c6fe1e6ff80d45d5ece998cb733459a9
Mohammed Minhaj <nijulinux@gmail.com>
2025-02-28 00:45:47 +0530

    feat(zego-cloud): added basic zego cloud component

---
+++ src/app/features/project/pages/zego-cloud/zego-cloud.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zego-cloud',
+  imports: [],
+  templateUrl: './zego-cloud.component.html',
+  styleUrl: './zego-cloud.component.css'
+})
+export class ZegoCloudComponent {
+
+}
