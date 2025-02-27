06c9351eb51a48d8104d632a6828a124d4b545d9
Mohammed Minhaj <nijulinux@gmail.com>
2025-02-28 01:33:22 +0530

    ***

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  back_end: 'http://localhost:3000',
+  production: true,
+  CLAUDINARY_APPID: 1302389331,
+  CLAUDINARY_SERVER_SECRET: 'c3e1b984c7f277bddd6962d2c6e68399',
+};
