67b019ec5ba376d1268f9facca635e156362ebc0
Mohammed Minhaj <nijulinux@gmail.com>
2025-02-28 01:46:02 +0530

    fix(environment): fixed backend api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  back_end: 'http://localhost:3000',
+  back_end: 'https://api.teambase.mohammedminhaj.blog',
   production: true,
   CLAUDINARY_APPID: 1302389331,
   CLAUDINARY_SERVER_SECRET: 'c3e1b984c7f277bddd6962d2c6e68399',
