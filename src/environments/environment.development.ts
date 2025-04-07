e59c5587b6dc7c489ef158a890e6b874371990d1
Mohammed Minhaj <nijulinux@gmail.com>
2025-04-07 16:59:44 +0530

    updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 export const environment = {
-  // back_end: 'https://api.teambase.mohammedminhaj.blog',
   back_end: 'http://localhost:3000',
-  production: true,
+  production: false,
   CLAUDINARY_APPID: 1302389331,
   CLAUDINARY_SERVER_SECRET: 'c3e1b984c7f277bddd6962d2c6e68399',
 };
