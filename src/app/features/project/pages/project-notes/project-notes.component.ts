7d735e12ef69f772dd16ff3acce849f2dda67a2e
Mohammed Minhaj <nijulinux@gmail.com>
2025-04-11 19:10:14 +0530

    feat(project): project notes done

---
+++ src/app/features/project/pages/project-notes/project-notes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-notes',
+  imports: [],
+  templateUrl: './project-notes.component.html',
+  styleUrl: './project-notes.component.css'
+})
+export class ProjectNotesComponent {
+
+}
