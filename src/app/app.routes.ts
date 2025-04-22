d262fa97c967c88ced5d4356138baa9679f8e75a
Mohammed Minhaj <nijulinux@gmail.com>
2025-04-22 18:57:01 +0530

    feat: main component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,20 @@ import { Routes } from '@angular/router';
 import { UserAuthGuard } from './core/guards/user.guard';
 import { HomeComponent } from './features/home/home-page/home.component';
 import { ProfileEditComponent } from './features/home/profile-edit/profile-edit.component';
+import { MainPageComponent } from './features/home/main-page/main-page.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: MainPageComponent
+  },
+  {
+    path: 'dashboard',
     component: HomeComponent,
     canActivate: [UserAuthGuard],
   },
   {
-    path: 'profile',
+    path: 'dashboard/profile',
     component: ProfileEditComponent,
     canActivate: [UserAuthGuard],
   },
